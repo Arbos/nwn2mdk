b8c5a6a6ec9e377299d3258f749d515cd001ee89
Jose M. Arbos <jmarbos@gmail.com>
2017-06-29 15:05:00 +0200

    Remove commented dead code

--- mdb2fbx/export_gr2.cpp
+++ mdb2fbx/export_gr2.cpp
@@ -227,9 +227,7 @@ static void export_animation(FbxScene *scene, GR2_animation *anim)
 {
 	auto anim_stack = FbxAnimStack::Create(scene, "Skeleton");
 	auto anim_layer = FbxAnimLayer::Create(scene, "Action");
-	anim_stack->AddMember(anim_layer);
-	//anim_layer->BlendMode.Set(FbxAnimLayer::eBlendAdditive);
-	//anim_layer->Weight.Set(0.01);
+	anim_stack->AddMember(anim_layer);	
 
 	for (int i = 0; i < anim->track_groups_count; ++i)
 		export_animation(scene, anim->track_groups[i], anim_layer);
