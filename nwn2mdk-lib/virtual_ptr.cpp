3e036834a3a0bb66bdb20b1b31fa3e4f2eb98c8f
Jose M. Arbos <jmarbos@gmail.com>
2019-01-22 03:07:18 +0100

    Fix virtual pointer issue
    The enconding of null pointers was wrong and made NWN2 to crash.

--- nwn2mdk-lib/virtual_ptr.cpp
+++ nwn2mdk-lib/virtual_ptr.cpp
@@ -12,15 +12,23 @@ static std::unordered_map<uint32_t, void*>& decoding_map()
 
 void* decode_ptr(uint32_t encoded_ptr)
 {
+#ifdef VIRTUAL_PTR
 	auto it = decoding_map().find(encoded_ptr);
 	if (it != decoding_map().end())
 		return it->second;
 
 	return nullptr;
+#else
+	return (void*)encoded_ptr;
+#endif
 }
 
 uint32_t encode_ptr(const void *p)
 {
+#ifdef VIRTUAL_PTR
+	if (!p)
+		return 0;
+
 	static std::unordered_map<const void*, uint32_t> encoding_map;
 
 	auto it = encoding_map.find(p);
@@ -33,4 +41,7 @@ uint32_t encode_ptr(const void *p)
 	decoding_map().emplace(encoded_ptr, const_cast<void*>(p));
 
 	return encoded_ptr;
+#else
+	return (uint32_t)p;
+#endif
 }
