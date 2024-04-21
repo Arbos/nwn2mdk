360ec66bcb70a252c125ba9d29488acd580bd711
Jose M. Arbos <jmarbos@gmail.com>
2024-04-21 10:29:15 +0200

    Blender: Update doc_url

--- blender-addon/__init__.py
+++ blender-addon/__init__.py
@@ -5,7 +5,7 @@ bl_info = {
     "blender": (2, 93, 7),
     "location": "File > Import-Export",
     "description": "Neverwinter Nights 2 MDB/GR2 Import/Export",
-    "doc_url": "https://github.com/Arbos/nwn2mdk/wiki",
+    "doc_url": "https://arbos.github.io/nwn2/blender",
     "category": "Import-Export",
 }
 
